1eb83db8de64a4695dd800e53198a0fbc2505527
zanderfh <87620213+zanderfh@users.noreply.github.com>
2022-04-12 17:41:07 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,48 +1,58 @@
 import datetime
-counter = 0
 
-file = open('input.txt')
+def runMain():
 
-content = file.readlines()
+    counter = 0
 
-with open("input.txt") as f:
-    for i, l in enumerate(f):
-        pass
-len = i + 1
+    file = open('devinput.txt')
 
+    content = file.readlines()
 
-x = range(1,len)
+    with open("devinput.txt") as f:
+        for i, l in enumerate(f):
+            pass
+    len = i + 1
 
-for i in x:
-    ln = content[i]
+    x = range(1,len)
 
-    cont = ln.strip()
-    dy = int(cont.split('/')[0])
-    mn = int(cont.split('/')[1])
-    yr = int(cont.split('/')[2])
+    for i in x:
+        ln = content[i]
 
-    date = datetime.datetime(yr,mn,dy)
-    yesterday = date + datetime.timedelta(days = -1)
+        cont = ln.strip()
+        dy = int(cont.split('/')[0])
+        mn = int(cont.split('/')[1])
+        yr = int(cont.split('/')[2])
 
-    frmt = str(date)[0:10]
-    frmt2 = str(yesterday)[0:10]
+        date = datetime.datetime(yr,mn,dy)
+        yesterday = date + datetime.timedelta(days = -1)
 
-    ln2 = content[i-1]
-    cont2 = ln2.strip()
+        frmt = str(date)[0:10]
+        frmt2 = str(yesterday)[0:10]
 
-    dy2 = int(cont2.split('/')[0])
-    mn2 = int(cont2.split('/')[1])
-    yr2 = int(cont2.split('/')[2])
+        ln2 = content[i-1]
+        cont2 = ln2.strip()
 
-    date2 = datetime.datetime(yr2,mn2,dy2)
-    frmt3 = str(date2)[0:10]
+        dy2 = int(cont2.split('/')[0])
+        mn2 = int(cont2.split('/')[1])
+        yr2 = int(cont2.split('/')[2])
 
-    if frmt2 == frmt3:
-        counter += 1
+        date2 = datetime.datetime(yr2,mn2,dy2)
+        frmt3 = str(date2)[0:10]
 
-    elif frmt2 != frmt3:
-        counter = 0
+        if frmt2 == frmt3:
+            counter += 1
 
-print(f"So far you have a streak of {counter} days!")
-print(f"You only need {365-counter} days until you get the Devotee badge!")
-input("Press ENTER to exit:  ")
+        elif frmt2 != frmt3:
+            counter = 0
+
+    print(f"So far you have a streak of {counter} days!")
+
+    if counter > 99:
+        print("Congratulations on the Aficionado badge!")
+
+    else:
+        print(f"You only need {100-counter} days until you get the Aficionado badge!")
+
+    print(f"You only need {365-counter} days until you get the Devotee badge!")
+
+runMain()
